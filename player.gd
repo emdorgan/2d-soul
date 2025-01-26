845e6765f502241f2ad3cea4ba1d14aa43fb6771
Emily <wmdorgan@gmail.com>
2025-01-25 16:33:26 -0800

    feat: weapon instantiation and deletion

--- player.gd
+++ player.gd
@@ -2,18 +2,26 @@ extends CharacterBody2D
 
 @export var speed: float = 300.0
 @onready var character_sprite: AnimatedSprite2D = $CharacterAnimation
+@onready var character_animation: AnimationPlayer = $AnimationPlayer
+
+var player_sword_scene : PackedScene = load("res://player_sword.tscn")
+var active_weapon : Area2D;
 
 var direction := Vector2.ZERO
 var last_direction := Vector2.DOWN
+var can_attack := true
 
-func _process(delta: float) -> void:
-	update_direction()
-	move_character(delta)
-	update_animation()
-
-func update_direction() -> void:
+func handle_input() -> void:
+	if Input.is_action_just_pressed("Slash") and can_attack:  
+		can_attack = false
+		active_weapon = player_sword_scene.instantiate()
+		$Weapon.add_child(active_weapon)
+		active_weapon.position = Vector2(0,0)
+		$AttackTimer.start()
+		play_attack_animation()
+		
 	direction = Vector2.ZERO
-
+	
 	if Input.is_action_pressed("Up"):
 		direction = Vector2.UP
 	elif Input.is_action_pressed("Down"):
@@ -22,11 +30,17 @@ func update_direction() -> void:
 		direction = Vector2.LEFT
 	elif Input.is_action_pressed("Right"):
 		direction = Vector2.RIGHT
-
+	
 	if direction != Vector2.ZERO:
 		last_direction = direction
 
-func move_character(_delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
+	handle_input()
+	move_character(delta)
+	update_animation()
+
+func move_character(delta: float) -> void:
 	velocity = direction * speed
 	move_and_slide()
 
@@ -52,22 +66,22 @@ func update_animation() -> void:
 			Vector2.RIGHT:
 				character_sprite.play("idle_right")
 
+func play_attack_animation() -> void:
 
-#func attack() -> void:
-	#Global.is_attacking = true
-	#perform_sword_slash()
-#
-#func perform_sword_slash() -> void:
-	#var sword = sword_scene.instantiate()
-	#add_child(sword)  # Attach the sword to the player
-	#sword.position = $SlashStartingPos.position
-	#sword.rotation_degrees = rotation_degrees + 20;
-#
-	#var sword_slash_tween = create_tween()
-	## Animate the sword's rotation in a quarter-circle arc (from right to front)
-	#sword_slash_tween.tween_property(sword, "rotation_degrees", rotation_degrees - 110, 0.3)
-	#sword_slash_tween.finished.connect(func(): 
-		#sword.queue_free()
-		#Global.is_attacking = false
-	#)
-	#
+	match last_direction:
+		Vector2.UP:
+			character_animation.play("up_attack")
+		Vector2.DOWN:
+			character_animation.play("down_attack")
+		Vector2.LEFT:
+			character_animation.play("left_attack")
+		Vector2.RIGHT:
+			character_animation.play("right_attack")
+	
+	
+
+
+func _on_attack_timer_timeout() -> void:
+	if ($Weapon.get_child_count() > 0):
+		$Weapon.remove_child(active_weapon)
+	can_attack = true
